cb63b61de54f9a4dc3dcd3f0d08fc5fd0c841f32
coreybrowndev <bcorey395@gmail.com>
2025-03-28 08:26:22 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HomePage from "./components/HomePage.vue";
+</script>
+
+<template>
+  <div class="app-container">
+    <HomePage />
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  width: 390px;
+  height: 844px;
+  background-color: #ffffff;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  padding: 20px;
+  color: black;
+}
+</style>
+
+<style>
+/* Global styles */
+body {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f0f0f0;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="home">
+    <header>
+      <h1>
+        Welcome back, <br />
+        Sam
+      </h1>
+      <div class="avatar"></div>
+    </header>
+    <div class="group-session">
+      <button>Group Session</button>
+    </div>
+
+    <div class="timer-container">
+      <h3>Focus Timer</h3>
+      <div>
+        <img src="@/assets/timer.png" />
+      </div>
+    </div>
+
+    <div class="buttons-container">
+      <button>Start</button>
+      <button>Pause</button>
+      <button>Task List</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  gap: 20px;
+}
+
+header {
+  text-align: left;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header h1 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0;
+  padding: 0;
+}
+
+.avatar {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  background-color: #ccc;
+}
+
+.group-session {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+.group-session button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.timer-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.timer-container h3 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin: 0;
+  padding: 0;
+}
+
+.timer-container div {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -50px;
+}
+
+.timer-container img {
+  width: 500px;
+  height: 500px;
+}
+
+.buttons-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  gap: 10px;
+}
+
+.buttons-container button {
+  padding: 10px 20px;
+  width: 80%;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+}
+
+.buttons-container button:last-child {
+  padding: 10px 20px;
+  width: 100%;
+  background: white;
+  border: 2px solid #007bff;
+  color: black;
+}
+</style>

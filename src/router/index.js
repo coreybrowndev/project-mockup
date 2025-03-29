4509f8de063c6883fbd66deca5f93efca8cdc79d
coreybrowndev <bcorey395@gmail.com>
2025-03-28 21:52:24 -0400

    Fix pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import TaskView from "@/views/TaskView.vue";
 import GroupSessionView from "@/views/GroupSessionView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
